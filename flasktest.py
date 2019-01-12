0f33c7d0564931d80045043ea83c9995f4f2196d
ExrEric <182355A@mymail.nyp.edu.sg>
2019-01-12 17:39:47 +0800

    ver 1.1

--- flasktest.py
+++ flasktest.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
-@app.route('/hello')
+@app.route('/')
 def hello():
- return 'Hello, World!'
+    return 'Hello, World!'
 if __name__ == '__main__':
  app.run(port='80')
